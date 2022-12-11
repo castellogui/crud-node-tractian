fd7850be45b7600860da6ae050ee561ba04aa470
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-11 12:04:16 -0300

    creating server.js

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+import app from "../src/app";
+import env from "dotenv";
+env.config({ path: ".env" });
+mongoose
+  .connect(`${process.env.MONGODB_URL}`)
+  .then(() => {
+    console.log("Connected with MongoDB");
+    app.listen(8080);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
