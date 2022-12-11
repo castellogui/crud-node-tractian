7d297756cbc41d967fa529e74932090bb6827882
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-11 12:04:28 -0300

    creating app.js

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/UserRoutes";
+
+const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRoutes);
+
+export default app;
