500452effb6db6042839e0797f50e3c8c860b193
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 11:08:27 -0300

    creating checkirregularitiesinuserobject method to keep a clean code and added edituser method

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -30,26 +30,7 @@ export const createUser = async (req: Request, res: Response) => {
       companyId: req.body.companyId,
     };
 
-    for (const atr in newUser) {
-      if (newUser[atr as keyof typeof newUser] == "" && !(atr == "avatar" || atr == "lastLogin")) {
-        res.status(422).send({ message: `Field '${atr}' must not be empty.` });
-        return;
-      }
-
-      if (
-        typeof newUser[atr as keyof typeof newUser] != "string" &&
-        !(atr == "created_at" || atr == "lastLogin")
-      ) {
-        res.status(422).send({ message: `Field '${atr}' must be a string.` });
-        return;
-      }
-    }
-
-    if (typeof newUser.created_at != "number" || typeof newUser.lastLogin != "number") {
-      res.status(422).send({ message: "Field created_at or lastLogin must be a Date type." });
-      return;
-    }
-
+    checkIrregularitiesInUserObject(res, newUser);
     await User.create(newUser);
     res.status(201).send({ message: "User created", newUser });
   } catch (error) {
@@ -60,3 +41,52 @@ export const createUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const editUser = async (req: Request, res: Response) => {
+  try {
+    let id = req.params.id;
+
+    const userInfo: user = {
+      name: req.body.name,
+      familyName: req.body.familyName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      avatar: req.body.avatar,
+      created_at: req.body.created_at,
+      lastLogin: req.body.lastLogin,
+      companyId: req.body.companyId,
+    };
+
+    checkIrregularitiesInUserObject(res, userInfo);
+    const updatedUser = await User.updateOne({ _id: id }, userInfo);
+    res.status(200).send({ message: "User created", updatedUser });
+  } catch (error) {
+    res.status(500).send({
+      message: "Some error has occurred while trying to edit an user.",
+      errorDetail: error,
+    });
+  }
+};
+
+function checkIrregularitiesInUserObject(res: Response, newUser: user) {
+  for (const atr in newUser) {
+    if (newUser[atr as keyof typeof newUser] == "" && !(atr == "avatar" || atr == "lastLogin")) {
+      res.status(422).send({ message: `Field '${atr}' must not be empty.` });
+      return;
+    }
+
+    if (
+      typeof newUser[atr as keyof typeof newUser] != "string" &&
+      !(atr == "created_at" || atr == "lastLogin")
+    ) {
+      res.status(422).send({ message: `Field '${atr}' must be a string.` });
+      return;
+    }
+  }
+
+  if (typeof newUser.created_at != "number" || typeof newUser.lastLogin != "number") {
+    res.status(422).send({ message: "Field created_at or lastLogin must be a Date type." });
+    return;
+  }
+}
