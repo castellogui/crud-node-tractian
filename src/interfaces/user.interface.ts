0b90046ec57776669d9c4291629ac97fb91d3198
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 04:22:58 -0300

    creating user interface

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface user {
+  name: String;
+  familyName: String;
+  username: String;
+  email: String;
+  password: String;
+  avatar: String;
+  created_at: Date;
+  lastLogin: Date;
+  companyId: String;
+}
