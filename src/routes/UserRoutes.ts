df3489328325e074de004fe52a5e33129e481316
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 11:06:48 -0300

    adding a new route to user (edit method)

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import { validateRequest } from "../middleware/auth";
-import { findAllUsers, createUser } from "../services/UserService";
+import { findAllUsers, createUser, editUser } from "../services/UserService";
 const router = require("express").Router();
 
 router.get("/find", validateRequest, findAllUsers);
 
 router.post("/new", validateRequest, createUser);
+
+router.patch("/edit/:id", validateRequest, editUser);
 
 export default router;
