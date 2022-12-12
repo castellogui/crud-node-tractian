334e2f592ca3774147094716d784d76265f34ab1
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 04:24:13 -0300

    organizing user routes

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,32 +1,9 @@
-import { Request, Response } from "express";
-import User from "../models/User";
+import { validateRequest } from "../middleware/auth";
+import { findAllUsers, createUser } from "../services/UserService";
 const router = require("express").Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { name, familyName, username, email, password, avatar, created_at, company } = req.body;
+router.get("/find", validateRequest, findAllUsers);
 
-  if (!req.body) {
-    res.status(422).send({ msg: "Body request empty." });
-  }
-
-  const user = {
-    name,
-    familyName,
-    username,
-    email,
-    password,
-    avatar,
-    created_at,
-    company,
-  };
-
-  try {
-    await User.create(user);
-    res.status(201).send({ msg: "User created", user });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-});
+router.post("/new", validateRequest, createUser);
 
 export default router;
