1a5fb264f4340977427dc3c96c461b86e8cda3f6
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 22:27:14 -0300

    creating company services

---
+++ src/services/CompanyService.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export const findAllCompanies = (req: Request, res: Response) => {};
+
+export const findCompany = (req: Request, res: Response) => {};
+
+export const createCompany = (req: Request, res: Response) => {};
+
+export const editCompany = (req: Request, res: Response) => {};
+
+export const deleteCompany = (req: Request, res: Response) => {};
