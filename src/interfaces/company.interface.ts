ed5b9ecf8f7f8160130c6b9d6e4f65c33604c906
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-13 18:08:06 -0300

    changing company interface atrs to array

--- src/interfaces/company.interface.ts
+++ src/interfaces/company.interface.ts
@@ -1,6 +1,8 @@
+import mongoose from "mongoose";
+
 export interface company {
   name: String;
-  units: Array<String>;
-  users: Array<String>;
+  units: Array<mongoose.Types.ObjectId>;
+  users: Array<mongoose.Types.ObjectId>;
   created_at: Date;
 }
