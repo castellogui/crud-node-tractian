0999e0bb624dd946a524f8cf375f4a94cbe3e1d6
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 22:27:28 -0300

    adding companies routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/UserRoutes";
+import companyRoutes from "./routes/CompanyRoutes";
 
 const app = express();
 app.use(
@@ -12,5 +13,6 @@ app.use(
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRoutes);
+app.use("/companies", companyRoutes);
 
 export default app;
