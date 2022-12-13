68fbd29517d19f435359d8b8884400567802ab20
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-12 23:15:13 -0300

    creating utils for entities

---
+++ src/utils/entity.ts
@@ -0,0 +1,12 @@
+import { Response } from "express";
+
+export function handleEntityNotFoundOrNotModified(model: any, res: Response) {
+  if (model.matchedCount === 0) {
+    res.status(200).send({ message: "Entity not found." });
+    return true;
+  }
+
+  if (model.modifiedCount === 0) {
+    res.status(200).send({ message: "Nothing changed in user record." });
+  }
+}
