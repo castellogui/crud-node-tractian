f6fecadd81c70683da8eda67c4521153e9ea7adf
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-14 19:15:14 -0300

    adding assets routes to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import userRoutes from "./routes/UserRoutes";
 import companyRoutes from "./routes/CompanyRoutes";
 import unitRoutes from "./routes/UnitRoutes";
+import assetRoutes from "./routes/AssetRoutes";
 
 const app = express();
 app.use(
@@ -16,5 +17,6 @@ app.use(cors());
 app.use("/companies", companyRoutes);
 app.use("/users", userRoutes);
 app.use("/units", unitRoutes);
+app.use("/assets", assetRoutes);
 
 export default app;
