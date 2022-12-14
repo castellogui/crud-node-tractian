f14aacd9006cf1514dd3bf68e0cb64965d4bacbe
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-14 20:31:04 -0300

    adding export on createjwt method

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
-const createJWT = (data: String) => {
+export const createJWT = (data: String) => {
   let token = jwt.sign({ data }, `${process.env.JWT_KEY}`, { expiresIn: "1h" });
   return token;
 };
