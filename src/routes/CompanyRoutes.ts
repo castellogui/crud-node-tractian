ab5bd37b7692cb9179dd78f58004ca3b79992bea
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-13 23:53:44 -0300

    adding controller in routes

--- src/routes/CompanyRoutes.ts
+++ src/routes/CompanyRoutes.ts
@@ -5,15 +5,15 @@ import {
   createCompany,
   editCompany,
   deleteCompany,
-  moveUserFromCompany as moveUserFromCompany,
-} from "../services/CompanyService";
+  moveUserFromCompany,
+} from "../controllers/CompanyController";
 const router = require("express").Router();
 
 router.get("/find", validateRequest, findAllCompanies);
 router.get("/find/:id", validateRequest, findCompany);
 router.post("/new", validateRequest, createCompany);
-router.post("/moveUser/:id", validateRequest, moveUserFromCompany);
 router.put("/edit/:id", validateRequest, editCompany);
+router.put("/moveUser/:id", validateRequest, moveUserFromCompany);
 router.delete("/delete/:id", validateRequest, deleteCompany);
 
 export default router;
