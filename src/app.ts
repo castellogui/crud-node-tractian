5d2c2a78f2c8fabb91c2428e5aede70a937e1e17
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-14 01:23:26 -0300

    add middleware in app to call unit routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/UserRoutes";
 import companyRoutes from "./routes/CompanyRoutes";
+import unitRoutes from "./routes/UnitRoutes";
 
 const app = express();
 app.use(
@@ -14,5 +15,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/companies", companyRoutes);
 app.use("/users", userRoutes);
+app.use("/units", unitRoutes);
 
 export default app;
