433104c01b4c40acd2422cd8e16445eb0d659e2e
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-14 19:13:32 -0300

    creating asset model

---
+++ src/models/Asset.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, SchemaTypes, Types } from "mongoose";
+
+const AssetSchema = new Schema({
+  name: {
+    type: SchemaTypes.String,
+    required: [true, "name is required"],
+  },
+  unit: {
+    type: SchemaTypes.ObjectId,
+    required: [true, "unit is required"],
+  },
+  description: {
+    type: String,
+    required: [true, "description is required"],
+  },
+  image: {
+    type: String,
+    required: [true, "image is required"],
+  },
+  model: {
+    type: String,
+    required: [true, "model is required"],
+  },
+  owner: {
+    type: SchemaTypes.ObjectId,
+    required: [true, "owner is required"],
+  },
+  status: {
+    type: String,
+    enum: ["Running", "Alerting", "Stopped"],
+    required: [true, "status is required"],
+  },
+  healthLevel: {
+    type: String,
+    required: [true, "status is required"],
+  },
+  created_at: {
+    type: Date,
+    required: [true, "created_at is required"],
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Asset", AssetSchema);
