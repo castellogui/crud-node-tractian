8a5069f9e6437f87b642efa837760a05e204277c
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-14 14:19:57 -0300

    creating verifications in delete method to check company existence

--- src/controllers/UnitController.ts
+++ src/controllers/UnitController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import handleRequestError from "../utils/error";
 import UnitService from "../services/UnitService";
-import { unit } from "../interfaces/unit.interface";
+import { unit, updatedUnit } from "../interfaces/unit.interface";
 import { checkEntityNotFoundOrNotModified } from "../utils/entity";
 import CompanyService from "../services/CompanyService";
 const bcrypt = require("bcrypt");
@@ -37,11 +37,12 @@ export const createUnit = async (req: Request, res: Response) => {
       zipCode: req.body.zipCode,
       created_at: req.body.created_at,
       assets: req.body.assets,
-      company: req.body.company,
+      company: req.body.companyId,
     };
 
+    checkEntityNotFoundOrNotModified(await CompanyService.findCompany(newUnitInfo.company));
     let newUnit = await UnitService.createUnit(newUnitInfo);
-    await CompanyService.addUnitInCompany(newUnitInfo.company.toString(), newUnit._id.toString());
+    await CompanyService.addUnitInCompany(newUnitInfo.company, newUnit._id);
     res.status(201).send({ message: "Unit created.", newUnit });
   } catch (error) {
     handleRequestError(error, res, "create", "a new unit");
@@ -51,14 +52,16 @@ export const createUnit = async (req: Request, res: Response) => {
 export const editUnit = async (req: Request, res: Response) => {
   try {
     let id = req.params.id;
-    const unitInfo: unit = {
+    const unitInfo: updatedUnit = {
       name: req.body.name,
       zipCode: req.body.zipCode,
       created_at: req.body.created_at,
       assets: req.body.assets,
-      company: req.body.company,
+      company: req.body.companyId,
     };
 
+    checkEntityNotFoundOrNotModified(await CompanyService.findCompany(unitInfo.company));
+    //TODO check entity in assets
     let updatedUnit = await UnitService.editUnit(id, unitInfo);
     checkEntityNotFoundOrNotModified(updatedUnit);
     res.status(200).send({ message: "Unit updated.", updatedUnit });
@@ -70,8 +73,10 @@ export const editUnit = async (req: Request, res: Response) => {
 export const deleteUnit = async (req: Request, res: Response) => {
   try {
     let id = req.params.id;
-    let requesterId = req.body.requesterId;
-    await UnitService.deleteUnit(id, requesterId);
+    let companyId = req.body.companyId;
+    await UnitService.deleteUnit(id);
+    checkEntityNotFoundOrNotModified(await CompanyService.findCompany(companyId));
+    await CompanyService.removeUnitFromCompany(id, companyId);
     res.status(200).send({ message: "Unit deleted." });
   } catch (error) {
     handleRequestError(error, res, "delete", "unit");
